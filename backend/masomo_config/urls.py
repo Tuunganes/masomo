6595dc6083f46e883a2e30b036cd87d3100f78c2
Marcel bag <marcelshabani@gmail.com>
2025-02-09 17:28:59 +0200

    new updates

--- backend/masomo_config/urls.py
+++ backend/masomo_config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include  # Ensure to import 'include'
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin panel URL
     path('', include('students.urls')),  # Include the students app URLs
+    
 ]
