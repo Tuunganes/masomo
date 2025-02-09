e301778810f4e7ecb0165e6f44936cfb6edd2310
Marcel bag <marcelshabani@gmail.com>
2025-02-09 15:15:27 +0200

    new page

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -5,3 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
     message.textContent = 'Welcome to your Django app!';
     document.body.appendChild(message);
 });
+// Fonction pour afficher la section correspondante lorsqu'on clique sur un onglet
+function showSection(sectionId) {
+    // Masquer toutes les sections
+    const sections = document.querySelectorAll('.tab-content');
+    sections.forEach(section => section.classList.remove('active'));
+
+    // Afficher la section sélectionnée
+    const activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
