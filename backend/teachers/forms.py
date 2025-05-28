574635e8bdc3c0e0ac654a49d26ca37016fa75db
Marcel bag <marcelshabani@gmail.com>
2025-05-28 20:04:58 +0200

    updating config to take teacher views

--- backend/teachers/forms.py
+++ backend/teachers/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Teacher
+from django_flatpickr.widgets import DatePickerInput
 
 class TeacherForm(forms.ModelForm):
     class Meta:
         model = Teacher
-        fields = ['full_name', 'gender', 'subject', 'phone', 'email']
+        fields = [
+            'full_name', 'gender', 'date_of_birth', 'nationality', 'address',
+            'subject', 'qualifications', 'phone', 'email', 'emergency_contact',
+            'status', 'profile_picture'
+        ]
+        widgets = {
+            'date_of_birth': DatePickerInput(),
+        }
