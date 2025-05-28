979e5f672373a2d40b163f789c942f2f9bdc6d5b
Marcel bag <marcelshabani@gmail.com>
2025-05-28 20:00:12 +0200

    updating config to take teacher views

--- backend/teachers/views.py
+++ backend/teachers/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Teacher
+from .forms import TeacherForm
+from django.contrib.auth.decorators import login_required
