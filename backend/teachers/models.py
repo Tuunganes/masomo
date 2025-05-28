b67d1c245a9c12c4ba70df4d98be43193d6b3c9b
Marcel bag <marcelshabani@gmail.com>
2025-05-28 19:46:05 +0200

    updating config to take teachers gender

--- backend/teachers/models.py
+++ backend/teachers/models.py
@@ -1,4 +1,16 @@
 from django.db import models
-
-from django.db import models
 from django.utils.text import slugify
+
+class Teacher(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    full_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    subject = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True)
