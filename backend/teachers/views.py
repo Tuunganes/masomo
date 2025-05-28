92bbd99987d884a44c2f90bba095e7d07fafa814
Marcel bag <marcelshabani@gmail.com>
2025-05-28 20:00:34 +0200

    updating config to take teacher views

--- backend/teachers/views.py
+++ backend/teachers/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Teacher
 from .forms import TeacherForm
 from django.contrib.auth.decorators import login_required
+
+@login_required
+def teacher_list(request):
+    teachers = Teacher.objects.all()
+    return render(request, 'teacher_list.html', {'teachers': teachers})
