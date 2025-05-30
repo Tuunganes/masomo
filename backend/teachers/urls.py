52188f203dc05d45056fbd5cfb1ed7f02ffa40cf
Marcel bag <marcelshabani@gmail.com>
2025-05-30 16:29:07 +0200

    updating teachers instance

--- backend/teachers/urls.py
+++ backend/teachers/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'teachers'
 
 urlpatterns = [
-    path('', views.teacher_list, name='teacher_list'),
-    path('add/', views.add_teacher, name='add_teacher'),
-    path('<slug:slug>/', views.teacher_detail, name='teacher_detail'),
-    path('<slug:slug>/edit/', views.edit_teacher, name='edit_teacher'),
-    path('<slug:slug>/delete/', views.delete_teacher, name='delete_teacher'),
+    path('',                   views.teacher_list,   name='teacher_list'),
+    path('add/',               views.teacher_add,    name='teacher_add'),
+    path('<slug:slug>/',       views.teacher_detail, name='teacher_detail'),
+    path('<slug:slug>/edit/',  views.teacher_edit,   name='teacher_edit'),
+    path('<slug:slug>/delete/',views.teacher_delete, name='teacher_delete'),
 ]
