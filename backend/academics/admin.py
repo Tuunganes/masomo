5f0ea01a0a3cc0175be75da47aac5b7c106f18c6
Marcel bag <marcelshabani@gmail.com>
2025-05-31 11:45:15 +0200

    writting academics admin

--- backend/academics/admin.py
+++ backend/academics/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import AcademicYear, SchoolClass, Subject
 
-# Register your models here.
+@admin.register(AcademicYear)
+class AcademicYearAdmin(admin.ModelAdmin):
+    list_display  = ("name", "start_date", "end_date", "is_current")
+    list_editable = ("is_current",)
+
+@admin.register(SchoolClass)
+class SchoolClassAdmin(admin.ModelAdmin):
+    list_display  = ("name", "academic_year", "main_teacher", "room")
+    list_filter   = ("academic_year",)
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display  = ("name", "code", "school_class", "teacher")
+    list_filter   = ("school_class__academic_year",)
