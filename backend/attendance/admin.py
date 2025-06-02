6cfada6424f01fbc5141fa8c4385d91532829aa4
Marcel bag <marcelshabani@gmail.com>
2025-06-02 20:37:28 +0200

    Setting Attendance admin

--- backend/attendance/admin.py
+++ backend/attendance/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+# backend/attendance/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Attendance
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ("student", "date", "status", "marked_by")
+    list_filter  = ("date", "status")
+    search_fields = (
+        "student__first_name",
+        "student__last_name",
+        "marked_by__first_name",
+        "marked_by__last_name",
+    )
